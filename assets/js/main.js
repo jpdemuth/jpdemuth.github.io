8c019e43d853ee1f8d81915efe3c9406bdca471a
Jeff Demuth <jpdemuth@uta.edu>
2023-07-26 15:59:00 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,19 @@
 */
 (function() {
   "use strict";
-
+  
+ // Hero typed
+  if ($('.typed').length) {
+    var typed_strings = $(".typed").data('typed-items');
+    typed_strings = typed_strings.split(',')
+    new Typed('.typed', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 10,
+      backDelay: 2000
+    });
+  }
   /**
    * Easy selector helper function
    */
